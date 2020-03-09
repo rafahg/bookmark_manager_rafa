0871cfd4f4e022413037737f5755043639a7c955
Student <student@Makerss-MacBook-Air.local>
2020-03-09 17:35:23 +0000

    code refactor with class Bookmark and readme.md updated

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 # Client -> Controller -> Model -> View
 
 # Controller -> View 
+
+ - At this step we created the Bookmark class and we have refactored the code to call that 
+ class and the method self.all.
+
+ Every test is passing at the moment.
--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
-class Bookmark < Sinatra::Base 
+class BookmarkManager < Sinatra::Base 
+  enable :sessions
+
   get '/' do
-    # "Bookmark Manager"
-      erb :index
+    "Bookmark Manager"
+  end
+
+  get '/bookmarks' do
+    # session[:bookmarks] = BookmarkManager.new.show_bookmarks
+    # erb :bookmarks
+    @bookmarks = ["http://www.makersacademy.com",
+    "http://www.destroyallsoftware.com",
+    "http://www.google.com"]
+    # bookmarks.join
+    erb :'bookmarks/index'
   end
   
   run! if app_file == $0
